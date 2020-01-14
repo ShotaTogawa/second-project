52046c409f276f56d4075136f7f1443307e9acfe
ShotaTogawa <s.togawa0112@gmail.com>
2020-01-13 19:50:41 -0800

    made wireframe

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import classes from "./profile.css";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <div className="Profile" style={classes.Profile}></div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
